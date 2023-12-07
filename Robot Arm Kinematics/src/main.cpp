43bfb8a80d57671ee5f12634fa5c4bca01f6ef3d
Jose Lara <109773791+JosFlame11@users.noreply.github.com>
2023-12-07 02:07:56 -0600

    Added functinos

--- Robot Arm Kinematics/src/main.cpp
+++ Robot Arm Kinematics/src/main.cpp
@@ -14,9 +14,56 @@ ServoEasing J2(0x40);
 ServoEasing J3(0x40);
 ServoEasing J4(0x40);
 
+bool stringComplete = false;
+String inputString = "";
+
+
+void readSerial(){
+  while (Serial.available()){
+    char c = (char)Serial.read();
+    if(c != '\n'){
+      stringComplete = true;
+    } 
+    else{
+      inputString += c;
+    }
+  }
+}
+
+void doCommand(){
+
+  //This commands are for joint to joint movement
+  if (inputString.indexOf("$S1") != -1){
+    String degree = inputString.substring(2);
+    int d = degree.toInt();
+    J1.startEaseTo(d, 20);
+    }
+  else if (inputString.indexOf("$S2") != -1){
+    String degree = inputString.substring(2);
+    int d = degree.toInt();
+    J2.startEaseTo(d, 20);
+    }
+  else if (inputString.indexOf("$S3") != -1){
+    String degree = inputString.substring(2);
+    int d = degree.toInt();
+    J3.startEaseTo(d, 20);
+    }
+  else if (inputString.indexOf("$S4") != -1){
+    String degree = inputString.substring(2);
+    int d = degree.toInt();
+    J4.startEaseTo(d, 20);
+    }
+
+    //this commands are for Linear movement, this will only affect the end effector in x and z
+
+
+}
+
 
 
 void setup() {
+  Serial.begin(9600);
+
   J1.attach(0, START_DEGREE_VALUE, 680, 2350);
   J2.attach(1, START_DEGREE_VALUE, 850, 2500);
   J3.attach(2, START_DEGREE_VALUE, 400, 2150);
@@ -26,7 +73,15 @@ void setup() {
 }
 
 void loop() {
-    J1.startEaseTo(90, 20);
+
+    if(stringComplete){
+      inputString.trim();
+      Serial.println();
+
+
+      inputString = "";
+      stringComplete = false;
+    }
     J2.startEaseTo(90, 30);
     J3.startEaseTo(90, 30);
     J4.startEaseTo(90, 20);
