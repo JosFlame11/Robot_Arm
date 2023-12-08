3c728bb10fb703f8a169192ba6a190860bcf322a
Jose Lara <109773791+JosFlame11@users.noreply.github.com>
2023-12-08 13:27:48 -0600

    Add files via upload
    added linear movement, kinda

--- Robot Arm Kinematics/src/main.cpp
+++ Robot Arm Kinematics/src/main.cpp
@@ -16,46 +16,72 @@ ServoEasing J4(0x40);
 
 bool stringComplete = false;
 String inputString = "";
+String data;
 
 
-void readSerial(){
-  while (Serial.available()){
-    char c = (char)Serial.read();
-    if(c != '\n'){
-      stringComplete = true;
-    } 
-    else{
-      inputString += c;
-    }
-  }
+//For linear movement
+const int L2 = 120;
+const int L1 = 120;
+
+
+
+int rad2Degree( float rad){
+  return (int)(rad * 180 / PI);
+  
 }
+void inverseKinematics(int z){
+  int x = 50;
+  int h = sqrt(x * x + z * z);
+
+  float theta = acos((h/2)/L1);
+  float phi = atan(x/z);
+
+  int a1 = rad2Degree(phi + theta);
+  int a2 = rad2Degree(phi - theta);
+  int ra2 = 90 - theta - a2; 
+  
+  J2.startEaseToD(a1, 50);
+  J3.easeTo(ra2, 200);  
+
+}
+
+
+
 
 void doCommand(){
 
   //This commands are for joint to joint movement
-  if (inputString.indexOf("$S1") != -1){
-    String degree = inputString.substring(2);
-    int d = degree.toInt();
-    J1.startEaseTo(d, 20);
+  if (data.indexOf("S1") != -1){
+    String degree = data.substring(2);
+    int dg = degree.toInt();
+    J1.startEaseTo(dg, 60);
     }
-  else if (inputString.indexOf("$S2") != -1){
-    String degree = inputString.substring(2);
-    int d = degree.toInt();
-    J2.startEaseTo(d, 20);
+  else if (data.indexOf("S2") != -1){
+    String degree = data.substring(2);
+    int dg = degree.toInt();
+    J2.startEaseTo(dg, 60);
     }
-  else if (inputString.indexOf("$S3") != -1){
-    String degree = inputString.substring(2);
-    int d = degree.toInt();
-    J3.startEaseTo(d, 20);
+  else if (data.indexOf("S3") != -1){
+    String degree = data.substring(2);
+    int dg = degree.toInt();
+    J3.startEaseTo(dg, 60);
     }
-  else if (inputString.indexOf("$S4") != -1){
-    String degree = inputString.substring(2);
-    int d = degree.toInt();
-    J4.startEaseTo(d, 20);
+  else if (data.indexOf("S4") != -1){
+    String degree = data.substring(2);
+    int dg = degree.toInt();
+    J4.startEaseTo(dg, 60);
     }
 
     //this commands are for Linear movement, this will only affect the end effector in x and z
+  else if (data.indexOf("LZ") != -1){
+      String distance = data.substring(2);
+      if (distance = 1){
+        inverseKinematics(50);
+        inverseKinematics(120);
 
+      }
+
+    }
 
 }
 
@@ -69,23 +95,19 @@ void setup() {
   J3.attach(2, START_DEGREE_VALUE, 400, 2150);
   J4.attach(3, START_DEGREE_VALUE, 500, 2350);
   delay(500);
+  J3.setReverseOperation(true);
   
 }
 
 void loop() {
 
-    if(stringComplete){
-      inputString.trim();
-      Serial.println();
+    if (Serial.available()){
+    data = Serial.readString();
+    data.trim();
 
+    Serial.println(data);
 
-      inputString = "";
-      stringComplete = false;
-    }
-    J2.startEaseTo(90, 30);
-    J3.startEaseTo(90, 30);
-    J4.startEaseTo(90, 20);
-    delay(1000);
+    doCommand();
 
+  }
 }
-
